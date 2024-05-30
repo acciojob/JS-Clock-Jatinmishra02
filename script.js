88fab47b2e524a2deb3697fd994970c4f620e586
Jatin Mishra <104919571+Jatinmishra02@users.noreply.github.com>
2024-05-31 01:01:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your code here
 const secondHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
@@ -7,15 +6,15 @@ function setDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90; // Adding 90 to offset initial 90deg rotation
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90; // Adding 90 to offset initial 90deg rotation
+  const minsDegrees = ((mins / 60) * 360) + 90;
   minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
   const hour = now.getHours();
-  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90; // Adding 90 to offset initial 90deg rotation
+  const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
